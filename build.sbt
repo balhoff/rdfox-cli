645decd8a05466efe661e2d26466f453f25fbe4b
Jim Balhoff <jim@balhoff.org>
2016-08-24 12:20:56 -0400

    Initial commit.

---
+++ build.sbt
@@ -0,0 +1,36 @@
+enablePlugins(JavaAppPackaging)
+
+organization  := "org.rti.bioinformatics"
+
+name          := "rdfox-cli"
+
+version       := "0.0.1"
+
+scalaVersion  := "2.11.8"
+
+mainClass in Compile := Some("org.rti.bioinformatics.rdfox.Main")
+
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+scalacOptions in Test ++= Seq("-Yrangepos")
+
+resolvers += Resolver.mavenLocal
+
+javaOptions += "-Xmx12G"
+
+// Download JRDFox.jar and the associated native library for your platform
+// from https://www.cs.ox.ac.uk/isg/tools/RDFox/otherOS_download.html
+// Place these in `lib`
+
+libraryDependencies ++= {
+  Seq(
+    "org.backuity.clist"          %% "clist-core"            % "3.2.1",
+    "org.backuity.clist"          %% "clist-macros"          % "3.2.1" % "provided",
+    "net.sourceforge.owlapi"      %  "owlapi-distribution"   % "3.5.4",
+    "org.apache.directory.studio" %  "org.apache.commons.io" % "2.4",
+    "org.scalaz"                  %% "scalaz-core"           % "7.2.1",
+    "com.typesafe.scala-logging"  %% "scala-logging"         % "3.4.0",
+    "ch.qos.logback"              %  "logback-classic"       % "1.1.7",
+    "org.codehaus.groovy"         %  "groovy-all"            % "2.4.6"
+  )
+}
---
+++ src/main/scala/org/rti/bioinformatics/rdfox/Main.scala
@@ -0,0 +1,78 @@
+package org.rti.bioinformatics.rdfox
+
+import org.backuity.clist._
+import java.io.File
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.IRI
+import uk.ac.ox.cs.JRDFox.store.DataStore
+import uk.ac.ox.cs.JRDFox.store.DataStore.EqualityAxiomatizationType
+import uk.ac.ox.cs.JRDFox.store.DataStore.UpdateType
+import uk.ac.ox.cs.JRDFox.store.DataStore.Format
+import java.util.Date
+import org.apache.commons.io.FileUtils
+import scala.collection.JavaConverters._
+
+object Main extends CliMain[Unit](
+  name = "rdfox-cli",
+  description = "a command line wrapper for RDFox") {
+
+  var ontOpt = opt[Option[String]](name = "ontology", description = "OWL ontology to import into reasoning rules")
+  var storeFileOpt = opt[Option[File]](name = "store", description = "save the current state of the store to file")
+  var threadsOpt = opt[Option[Int]](name = "threads", description = "number of threads for parallel processing")
+  var exportFileOpt = opt[Option[File]](name = "export", description = "export RDF triples to Turtle file")
+  var reason = opt[Boolean](default = false, description = "apply reasoning after importing rules and data")
+  var dataFolderOpt = opt[Option[File]](name = "data", description = "Folder of RDF data files in Turtle format")
+
+  def run: Unit = {
+    import Util.time
+    val dataStore = storeFileOpt.filter(_.exists).map(new DataStore(_))
+      .getOrElse(new DataStore(DataStore.StoreType.ParallelComplexNN, EqualityAxiomatizationType.NoUNA))
+
+    val ontIRIOpt = ontOpt.map { ontPath => if (ontPath.startsWith("http")) IRI.create(ontPath) else IRI.create(new File(ontPath)) }
+    ontIRIOpt.foreach { ontIRI =>
+      val manager = OWLManager.createOWLOntologyManager()
+      val ontology = time("Loaded ontology from file") {
+        manager.loadOntology(ontIRI)
+      }
+      time("Imported ontology into RDFox rules") {
+        dataStore.importOntology(ontology, UpdateType.Add, true, true)
+      }
+    }
+    threadsOpt.foreach(dataStore.setNumberOfThreads)
+    dataFolderOpt.foreach { dataFolder =>
+      val datafiles = FileUtils.listFiles(dataFolder, null, true).asScala.filterNot(_.getName == "catalog-v001.xml").filter(_.isFile).toArray
+      time("Imported data files") {
+        dataStore.importFiles(datafiles)
+      }
+    }
+    if (reason) {
+      time("Applied reasoning") {
+        dataStore.applyReasoning()
+      }
+    }
+    storeFileOpt.foreach { storeFile =>
+      time("Saved database to file") {
+        dataStore.save(storeFile)
+      }
+    }
+    exportFileOpt.foreach { exportFile =>
+      time("Exported data to Turtle") {
+        dataStore.export(exportFile, Format.Turtle)
+      }
+    }
+    dataStore.dispose()
+  }
+
+}
+
+object Util {
+
+  def time[T](action: String)(f: => T): T = {
+    val s = System.currentTimeMillis
+    val res = f
+    val time = (System.currentTimeMillis - s) / 1000.0
+    println(s"$action in ${time}s")
+    res
+  }
+
+}
